6ceaf9e55962c0a31d23e03b90d0761c57858668
CyberAP <stasvarenkin@gmail.com>
2018-11-06 22:41:30 +0300

    Always replace confusing characters

--- preventMisreadings.js
+++ preventMisreadings.js
@@ -1,25 +1,12 @@
-var confusingCharPairs = [['1', 'l'], ['0', 'o']];
-var hasSymbol = function(symbol) {
-    return function(str) {
-        return str.indexOf(symbol) > -1;
-    };
-};
+var confusingChars = ['1', 'l', '0', 'o'];
 var matchAllOccurences = function(symbol) {
     return new RegeExp(symbol + '+', 'gi');
 };
 
-module.exports = function(str, pairs) {
-    pairs = pairs || confusingCharPairs;
-    pairs.forEach(function(pair) {
-        var leftSymbol = pair[0];
-        var rightSymbol = pair[1];
-        var hasLeftSymbol = hasSymbol(leftSymbol);
-        var hasRightSymbol = hasSymbol(rightSymbol);
-        if (hasLeftSymbol(str) && hasRightSymbol(str)) {
-            str = str
-                .replace(matchAllOccurences(leftSymbol), '')
-                .replace(matchAllOccurences(rightSymbol), '');
-        }
+module.exports = function(str, chars) {
+    chars = chars || confusingChars;
+    chars.forEach(function(char) {
+        str = str.replace(matchAllOccurences(char), '');
     })
     return str;
 }
