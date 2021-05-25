78def31aed6c9ac299c7a779cdf456e16e2561e7
James Whitney <james@whitney.io>
2021-05-25 11:58:25 +1000

    Update nanoid-dictionary.js

--- src/nanoid-dictionary.js
+++ src/nanoid-dictionary.js
@@ -1,4 +1,6 @@
 export { alphanumeric } from './alphanumeric';
+export { hexadecimalLowercase } from './hexadecimal-lowercase';
+export { hexadecimalUppercase } from './hexadecimal-uppercase';
 export { lowercase } from './lowercase';
 export { nolookalikes } from './nolookalikes';
 export { nolookalikesSafe } from './nolookalikes-safe';
